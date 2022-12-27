a7362da46135085ba056aff60db497a0ad49c8e8
Andy Suderman <andy@suderman.dev>
2022-12-27 14:44:36 -0700

    update some installs

--- keda/install.sh
+++ keda/install.sh
@@ -1,2 +1,3 @@
+helm repo add kedacore https://kedacore.github.io/charts
+helm repo up
 helm upgrade --install keda kedacore/keda --namespace keda --create-namespace --values values.yaml
-
--- kyverno/install.sh
+++ kyverno/install.sh
@@ -1,1 +1,3 @@
+helm repo add kyverno https://kyverno.github.io/kyverno/
+helm repo up
 helm upgrade --install kyverno kyverno/kyverno -n kyverno --create-namespace --values values.yaml
