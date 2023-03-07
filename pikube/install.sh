21189f6513db3aeb1dcde38dbc7faa3d8f1fc791
Andy Suderman <andy@suderman.dev>
2023-03-07 16:01:57 -0700

    install insights

---
+++ pikube/install.sh
@@ -0,0 +1,6 @@
+helm repo add fairwinds-stable https://charts.fairwinds.com/stable
+helm upgrade --install insights-agent fairwinds-stable/insights-agent -f values.yaml \
+  --set insights.base64Token=$INSIGHTS_PIKUBE_TOKEN \
+  --version "2.11.*" \
+  --create-namespace \
+  --namespace insights-agent
