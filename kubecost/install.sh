2d2000a946d2b9022ad2c7f047c11176687f2591
Andy Suderman <andy@suderman.dev>
2023-03-16 12:00:06 -0600

    updates

--- kubecost/install.sh
+++ kubecost/install.sh
@@ -4,7 +4,7 @@
 helm upgrade --install kubecost cost-analyzer \
   --repo https://kubecost.github.io/cost-analyzer/ \
   --namespace kubecost --create-namespace \
-  --set kubecostToken="YW5keUBzdWRlcm1hbi5kZXY=xm343yadf98" \
+  --set kubecostToken="${KUBECOST_TOKEN}" \
   --set global.prometheus.enabled=false \
   --set prometheus.nodeExporter.enabled=false \
   --set prometheus.serviceAccounts.nodeExporter.create=false \
