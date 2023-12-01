fed5d829b3dc0c482a1e4c691f84d62aebb3d5a1
Andy Suderman <andy@suderman.dev>
2023-12-01 12:06:07 -0700

    Install polaris

--- multi-cluster-scale/template-manifests.sh
+++ multi-cluster-scale/template-manifests.sh
@@ -3,3 +3,5 @@
 curl -LO https://github.com/cert-manager/cert-manager/releases/download/v1.13.2/cert-manager.crds.yaml
 mv cert-manager.crds.yaml ./manifests/cert-manager/
 helm template cert-manager -n cert-manager jetstack/cert-manager --version 1.13.2  | kubectl slice -f - -o ./manifests/cert-manager/
+
+helm template polaris -n polaris fairwinds-stable/polaris | kubectl slice -f - -o ./manifests/polaris/
