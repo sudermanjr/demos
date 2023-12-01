58ab7e792bc4be65c48c49f1dd6e294f78385d84
Andy Suderman <andy@suderman.dev>
2023-12-01 12:11:41 -0700

    add kyverno

--- multi-cluster-scale/template-manifests.sh
+++ multi-cluster-scale/template-manifests.sh
@@ -5,3 +5,7 @@ mv cert-manager.crds.yaml ./manifests/cert-manager/
 helm template cert-manager -n cert-manager jetstack/cert-manager --version 1.13.2  | kubectl slice -f - -o ./manifests/cert-manager/
 
 helm template polaris -n polaris fairwinds-stable/polaris --values polaris.values.yaml | kubectl slice -f - -o ./manifests/polaris/
+
+curl -LO https://github.com/kyverno/kyverno/releases/download/v1.10.0/install.yaml
+kubectl slice -f install.yaml -o ./manifests/kyverno/
+rm install.yaml
