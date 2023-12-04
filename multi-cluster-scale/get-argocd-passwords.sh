74ea46853bfbfd2ca5d553b09395c5b6671722a9
Andy Suderman <andy@suderman.dev>
2023-12-04 11:47:10 -0700

    try some stuff

---
+++ multi-cluster-scale/get-argocd-passwords.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+
+clusters=('us-east' 'us-west' 'eu-central')
+
+for cluster in ${clusters[@]}; do
+  echo "${cluster}:"
+  kubectl get secret argocd-initial-admin-secret -ojson -n argocd | jq .data.password -r | base64 -d
+  echo
+done
