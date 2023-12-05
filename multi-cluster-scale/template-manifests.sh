16b2332e43841a76f8b808aefa408c42f9ea238d
Andy Suderman <andy@suderman.dev>
2023-12-05 13:42:32 -0700

    template the CRDs

--- multi-cluster-scale/template-manifests.sh
+++ multi-cluster-scale/template-manifests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-curl -LO https://github.com/cert-manager/cert-manager/releases/download/v1.13.2/cert-manager.crds.yaml
-mv cert-manager.crds.yaml ./manifests/cert-manager/
-helm template cert-manager -n cert-manager jetstack/cert-manager --version 1.13.2  | kubectl slice -f - -o ./manifests/cert-manager/
+#curl -LO https://github.com/cert-manager/cert-manager/releases/download/v1.13.2/cert-manager.crds.yaml
+#mv cert-manager.crds.yaml ./manifests/cert-manager/
+helm template cert-manager -n cert-manager jetstack/cert-manager --version 1.13.2 --values cert-manager.values.yaml | kubectl slice -f - -o ./manifests/cert-manager/
 
 helm template polaris -n polaris fairwinds-stable/polaris --values polaris.values.yaml | kubectl slice -f - -o ./manifests/polaris/
 
