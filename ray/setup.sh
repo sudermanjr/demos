9e8fd85d6390374e3be946fbccfbb6de704b7815
Andy Suderman <andy@suderman.dev>
2025-04-24 10:10:17 -0600

    feat: update gpu nodepool

--- ray/setup.sh
+++ ray/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-helm install prometheus --namespace prometheus --create-namespace prometheus-community/kube-prometheus-stack
+helm upgrade --install prometheus --namespace prometheus --create-namespace prometheus-community/kube-prometheus-stack
 
 kubectl create secret generic hf-secret   --from-literal=hf_api_token=${HF_TOKEN}   --dry-run=client -o yaml | kubectl apply -f -
 
@@ -8,4 +8,11 @@ helm repo add kuberay https://ray-project.github.io/kuberay-helm/
 helm repo update
 
 # Install both CRDs and KubeRay operator v1.3.0.
-helm install kuberay-operator kuberay/kuberay-operator --version 1.3.0
+helm upgrade --install kuberay-operator kuberay/kuberay-operator --version 1.3.0
+
+# Install kueue
+helm upgrade --install kueue oci://registry.k8s.io/kueue/charts/kueue \
+  --version=0.11.3 \
+  --namespace  kueue-system \
+  --create-namespace \
+  --wait --timeout 300s
